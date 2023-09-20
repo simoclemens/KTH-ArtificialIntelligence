bc1d0964f9048e2ef9338f38592a22c386e5ac16
Jonas Rosengren <rosengrenjonas98@gmail.com>
2023-09-20 15:36:56 +0200

    refine hmm0

--- ASS2-HMMs/HMM0.py
+++ ASS2-HMMs/HMM0.py
@@ -1,6 +1,5 @@
 import sys
 
-
 def matrix_prod(mat1,mat2):
 
     res = [[0 for _ in range(len(mat2[0]))] for _ in range(len(mat1))]
@@ -12,48 +11,47 @@ def matrix_prod(mat1,mat2):
 
     return res
 
-#lines = open(sys.argv[1]).readlines()
+def load_matrix_line(l):
+    M = []
+    elems = l.strip().split(" ")
+    n_row = int(elems[0])
+    n_col = int(elems[1])
+    numbers = elems[2:]
+
+    for i in range(n_row):
+        tmp = []
+        for j in range(n_col):
+            tmp.append(float(numbers[i*n_col+j]))
+        M.append(tmp)
+
+    return M
+
+def format_output(m):
+    rows = len(m)
+    cols = len(m[0])
+
+    out = str(rows) + " " + str(cols) + " "
+    for i in range(len(m)):
+        for j in range(len(m[0])):
+            out += str(m[i][j]) + " "
+        out += "\n"
+    return out
+
 lines = sys.stdin.readlines()
 
 # transition matrix
-A = []
-elems1 = lines[0].strip().split(" ")
-n_row1 = int(elems1[0])
-n_col1 = int(elems1[1])
-numbers1 = elems1[2:]
+A = load_matrix_line(lines[0])
 
-for i in range(n_row1):
-    tmp = []
-    for j in range(n_col1):
-        tmp.append(float(numbers1[i*n_col1+j]))
-    A.append(tmp)
-
-# observations matrix
-B = []
-elems2 = lines[1].strip().split(" ")
-n_row2 = int(elems2[0])
-n_col2 = int(elems2[1])
-numbers2 = elems2[2:]
-
-for i in range(n_row2):
-    tmp = []
-    for j in range(n_col2):
-        tmp.append(float(numbers2[i*n_col2+j]))
-    B.append(tmp)
+# emission matrix
+B = load_matrix_line(lines[1])
 
 # initial probabilities
 elems3 = lines[2].strip().split(" ")
 p0 = [[float(i) for i in elems3[2:]]]
 
-x = matrix_prod(p0, A)
+p1 = matrix_prod(p0, A)
 
-res = matrix_prod(x,B)
-out = ""
-
-out += " " + str(len(res))
-out += " " + str(len(res[0]))
-
-for p in res[0]:
-    out += " " + str(p)
+observation_distribution = matrix_prod(p1, B)
+out = format_output(observation_distribution)
 
 print(out)
