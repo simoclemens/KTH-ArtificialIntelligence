6470f474e9d181c8bd35201d48d0401d93947a29
Jonas Rosengren <rosengrenjonas98@gmail.com>
2023-10-06 14:12:44 +0200

    add parameter tuning: 35pts

--- ASS3-RL/rl5/student_5.py
+++ ASS3-RL/rl5/student_5.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python3
 # Q learning learning rate
-alpha = 0
+alpha = 0.9
 
 # Q learning discount rate
-gamma = 0
+gamma = 0.3
 
 # Epsilon initial
-epsilon_initial = 1
+epsilon_initial = 0.5
 
 # Epsilon final
-epsilon_final = 1
+epsilon_final = 0.05
 
 # Annealing timesteps
-annealing_timesteps = 1
+annealing_timesteps = 10000
 
 # threshold
-threshold = 1e-1
+threshold = 1e-6
